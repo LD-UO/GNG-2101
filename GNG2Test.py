79c045952e8176f7d14229f8e5cdf04277525fb9
Pierre Allard <palla036@uottawa.ca>
2022-10-07 17:31:25 -0400

    Update GNG2Test.py

--- GNG2Test.py
+++ GNG2Test.py
@@ -6,9 +6,26 @@ import wx
 print('Imported GUI Handler')
 
 
+#UI Code
 app = wx.App()
 
 frame = wx.Frame(None, title='Prototype')
 frame.Show()
 
 app.MainLoop()
+
+#Open PDF/Read Text
+def OpenPDF():
+    
+
+
+#Organize text and dump to txt for edits
+def TextEditor():
+    
+
+
+#Convert edited text to mp3
+def AudioConvert():
+    
+
+
