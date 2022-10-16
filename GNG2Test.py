26c4e2b54d71273d4f0c6f905a5539e280ab2bb2
Pierre Allard <palla036@uottawa.ca>
2022-10-16 13:41:04 -0400

    comments

--- GNG2Test.py
+++ GNG2Test.py
@@ -6,7 +6,7 @@ import wx
 print('Imported GUI Handler')
 
 
-#UI Code
+#UI Code (Takes all user inputs and calls below functions)
 app = wx.App()
 
 frame = wx.Frame(None, title='Prototype')
@@ -15,16 +15,33 @@ frame.Show()
 app.MainLoop()
 
 #Open PDF/Read Text
+
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Inputs: Location of PDF and PDF File
+#Outputs: Text from pdf in array of strings
+#Description: Opens PDF from specified location, reads text and returns it
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 def OpenPDF():
-    
     return null
 
 #Organize text and dump to txt for edits
+
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Inputs: Array/String from OpenPDF()
+#Outputs: String/Array ready to send to Audio Convert
+#Description: Organizes text (by chapter), exports to txt, allows user edits, reads txt and returns edited strings
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 def TextEditor():
     return null
 
 
 #Convert edited text to mp3
+
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Input: String/Array + Location for outputs
+#Output: Audio file(s)
+#Description: Convert Strings to multiple audio files
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 def AudioConvert():
     return null
 
