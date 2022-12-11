598960227440bb95c677159ee1fa1bfe492a19de
Pierre Allard <palla036@uottawa.ca>
2022-12-11 14:26:03 -0500

    Organized
    Updated Readme

---
+++ Old Version/GNG2Test.py
@@ -0,0 +1,49 @@
+from gtts import gTTS
+print('Imported Text-to-Speech')
+import PyPDF2
+print('Imported PDF-to-Text')
+import wx
+print('Imported GUI Handler')
+
+#UI Code (Takes all user inputs and calls below functions)
+app = wx.App()
+
+frame = wx.Frame(None, title='Prototype')
+frame.Show()
+
+app.MainLoop()
+
+#Open PDF/Read Text
+
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Inputs: Location of PDF and PDF File
+#Outputs: Text from pdf in array of strings
+#Description: Opens PDF from specified location, reads text and returns it
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+def OpenPDF():
+    
+    return  
+
+#Organize text and dump to txt for edits
+
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Inputs: Array/String from OpenPDF()
+#Outputs: String/Array ready to send to Audio Convert
+#Description: Organizes text (by chapter), exports to txt, allows user edits, reads txt and returns edited strings
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def TextEditor(string):
+    return 
+
+#Convert edited text to mp3
+
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Input: String/Array + Location for outputs
+#Output: Audio file(s)
+#Description: Convert Strings to multiple audio files
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def AudioConvert():
+    return 
+
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+SPEAKABLES
+
+Created for:
 # GNG-2101
+Group A1.2
+
 Book Conversion to MP3
+
+Notes:
+v1 and v1_Console are functionally the same. Console version will display 
+a python console to allow for emergency shutdowns and debugging outputs
+
